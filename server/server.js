0273a5b7f659316ca2cd75e8a46ff8dbe5c5b869
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-08-28 17:14:32 +0530

    uploading image in the upload folder and saving it in the databse

--- server/server.js
+++ server/server.js
@@ -3,16 +3,20 @@ const connectDb = require('./configDb/db');
 const { login, signup, authenticateJWT, editUserInfo, changePassword } = require('./controllers/userController');
 const { seedDestinationsDatabase, getAllDestinations, getDestinationById, clearDestinationsDatabase } = require('./controllers/destinationsController');
 const {addAddress, fetchAddress, clearAddressDatabase} = require('./controllers/addressController');
+const { uploadProfilePic } = require('./controllers/imageUploadController');
+const multer = require('multer');
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 connectDb();
+
+
 
 // -------address controllers-------
 // clearAddressDatabase();
@@ -20,11 +24,13 @@ app.post('/api/addAddress', addAddress);
 app.get('/api/fetchAddress', fetchAddress);
 
 
+
 // ------destinations controllers------
 // seedDestinationsDatabase();
 // clearDestinationsDatabase();
 app.get('/api/getAllDestinations', getAllDestinations);
 app.get('/api/getDestinations/byId', getDestinationById);
+
 
 
 // ------user controllers------
@@ -36,6 +42,24 @@ app.get('/api/user', authenticateJWT, (req, res) =>
 });
 app.post('/api/editUserInfo', editUserInfo);
 app.post('/api/changePassword', changePassword);
+
+
+
+// ------upload profile pic------
+const storage=multer.diskStorage(
+{
+    destination: function(req, file, cb)
+    {
+        cb(null, 'uploads/');
+    }
+    ,
+    filename: function(req, file, cb)
+    {
+        cb(null, `${Date.now()}-${Math.round(Math.random()*1E9)}-${file.originalname}`);
+    }
+});
+const upload=multer({storage: storage});
+app.post('/api/uploadProfilePic', upload.single('profilePic'), uploadProfilePic);
 
 
 
